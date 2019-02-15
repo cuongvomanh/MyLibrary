eddda2c02c5382e537b3630694c7d98b8e3e460d
cuongvomanh <cuong.vomanh195@gmail.com>
2019-02-15 16:43:19 +0700

    init file

---
+++ local_update_version.sh
@@ -0,0 +1,12 @@
+# must run before:
+# source ~/mypython/computer_vision/p3env/bin/python3
+lib='zemcy'
+ve_python=python3
+ve_pip=pip3
+rm -r dist/
+nano setup.py
+$ve_python setup.py sdist bdist_wheel
+$ve_pip install $(find ./dist -type f -name "*.whl")
+#twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+#$ve_pip install --upgrade -i https://test.pypi.org/simple/ $lib
+#~/mypython/computer_vision/p3env/bin/pip3 install --upgrade -i https://test.pypi.org/simple/ zemcy
--- update_version.sh
+++ update_version.sh
@@ -1,11 +1,11 @@
 # must run before:
 # source ~/mypython/computer_vision/p3env/bin/python3
 lib='zemcy'
-p3env_python3=~/mypython/computer_vision/p3env/bin/python3
-p3env_pip3=~/mypython/computer_vision/p3env/bin/pip3
+ve_python=python3
+ve_pip=pip3
 rm -r dist/
 nano setup.py
-$p3env_python3 setup.py sdist bdist_wheel
-twine upload --repository-url https://test.pypi.org/legacy/ dist/*
-$p3env_pip3 install --upgrade -i https://test.pypi.org/simple/ $lib
+$ve_python setup.py sdist bdist_wheel
+#twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+#$ve_pip install --upgrade -i https://test.pypi.org/simple/ $lib
 #~/mypython/computer_vision/p3env/bin/pip3 install --upgrade -i https://test.pypi.org/simple/ zemcy
